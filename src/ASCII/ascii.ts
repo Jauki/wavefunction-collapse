67844e2270c9d29cdfd59b27b9bb424e9349e6ea
julianjauk <jaujuc18@htl-kaindorf.at>
2023-03-15 15:38:13 +0100

    bug: algorithm not working

--- src/ASCII/ascii.ts
+++ src/ASCII/ascii.ts
@@ -20,19 +20,18 @@ type Coordinate = {
 }
 
 function hasDuplicateCoordinate(arr: Coordinate[], coord: Coordinate): boolean {
-  return arr.some(item => item.x === coord.x && item.y === coord.y);
+  return coord === undefined ? false : arr.some(item => item.x === coord.x && item.y === coord.y);
 }
 
 
 const pickedCell = [] as Coordinate[];
 const propagatedCell = [] as Coordinate[];
-const entropiedCell = [] as Coordinate[];
 
 
 export function generateInitGrid(size: number): Grid {
-  const grid: Grid = [...Array(size+1)].map(e => Array(size+1));
-  for (let i = 0; i <= size; i++) {
-    for (let j = 0; j <= size; j++){
+   const grid: Grid = Array.from(Array(size), () => new Array(size));
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++){
       grid[i][j] = [Terrain.Land, Terrain.Sand, Terrain.Water];
     }
   }
@@ -46,16 +45,18 @@ function printGrid(grid: Grid): void {
 }
 
 const getRandomCoordinate = (size: number): Coordinate => {
-  const x = Math.floor(Math.random() * size );
-  const y = Math.floor(Math.random() * size );
+  const x = Math.floor(Math.random() * size + 1);
+  const y = Math.floor(Math.random() * size + 1);
   return { x, y };
 }
 
-let count = 0;
 
-
-function pickRandomUnpickedCell(grid: Grid, cell?: Coordinate | undefined) {
-  const coordiante = shannonEntropy(grid);
+function pickRandomUnpickedCell(grid: Grid) {
+  const coordiante = shannonEntropy(grid)
+  if (coordiante === undefined) {
+    console.log("Done!")
+    return;
+  }
   console.log("Biggest: " + coordiante.x + ", " + coordiante.y);
   const selected = grid[coordiante.y][coordiante.x];
   
@@ -66,10 +67,7 @@ function pickRandomUnpickedCell(grid: Grid, cell?: Coordinate | undefined) {
   grid = propagateGrid(grid);
   printGrid(grid);
   console.log("-----------------------------------")
-  if (count <= 4 ) {
-    count++;
-    pickRandomUnpickedCell(grid);
-  }
+  pickRandomUnpickedCell(grid);
 }
 
 
@@ -90,27 +88,30 @@ function getNeighbors(grid: Grid, target: Coordinate): Coordinate[] {
   return neighbors;
 }
 
-function setPropabilites(terrain: Terrain) {
+function setPropabilites(terrain: Terrain[]) {
   // cell -> L -> S oder L 
   // cell -> W -> S  oder W
   // cell -> S -> L oder W oder S
-  /// TODO: it overwrites the cell if it was already selected 
-  switch (terrain) {
-    case Terrain.Water: return [Terrain.Sand, Terrain.Water];
-    case Terrain.Sand: return [Terrain.Sand, Terrain.Water, Terrain.Land];
-    case Terrain.Land: return [Terrain.Sand, Terrain.Land];
+  if (terrain.length < 1) {
+    return terrain
+  } else {
+    switch (terrain[0]) {
+      case Terrain.Water: return [Terrain.Sand, Terrain.Water];
+      case Terrain.Sand: return [Terrain.Sand, Terrain.Water, Terrain.Land];
+      case Terrain.Land: return [Terrain.Sand, Terrain.Land];
+    }
   }
 }
 
 function propagateGrid(grid: Grid) {
-  for (let i = 0; i <= grid.length - 1; i++) {
-    for (let j = 0; j <= grid.length - 1; j++) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
       if (grid[i][j].length === 1
         && hasDuplicateCoordinate(pickedCell, {x: j, y: i})) {
         const currentCell = grid[i][j]
         const neighbors = getNeighbors(grid, { x: j, y: i });
         for (const neighbor of neighbors) {
-          grid[neighbor.y][neighbor.x] = setPropabilites(currentCell[0])
+          grid[neighbor.y][neighbor.x] = setPropabilites(currentCell)
         }
         propagatedCell.push({ x: j, y: i })
       }
@@ -124,42 +125,67 @@ type TerrainStates = {
   [k in  Terrain]: number;
 }
 
-
-
-function shannonEntropy(grid: Grid): Coordinate {
-  const entropy = [...Array(grid.length)].map(e => Array(grid.length));
+function shannonEntropy(grid: Grid) {
+  const entropy = [...Array(grid.length)].map(e => Array(grid[0].length).fill(0));
+  
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid.length; j++) {
-      const stateCounts: TerrainStates = { W: 0, L: 0, S: 0 }
+    for (let j = 0; j < grid[0].length; j++) {
+      const stateCounts: { [state: string]: number } = { W: 0, L: 0, S: 0 };
       
-      const neighbors = getNeighbors(grid, { x: j, y: i });
-      for (const neighbor of neighbors) {
-        for (const cell of grid[neighbor.y][neighbor.x]) {
-          stateCounts[cell]++;
+      // count the number of occurrences of each state in the cell
+      for (let k = 0; k < grid[i][j].length; k++) {
+        stateCounts[grid[i][j][k]]++;
+      }
+
+      // calculate the probability of each state
+      const totalStates = Object.values(stateCounts).reduce((sum, count) => sum + count, 0);
+      const stateProbabilities = Object.values(stateCounts).map(count => count / totalStates);
+
+      // calculate the Shannon Entropy of the cell
+      let cellEntropy = 0;
+      for (const p of stateProbabilities) {
+        if (p > 0) {
+          cellEntropy -= p * Math.log2(p);
         }
       }
-      // Calc Shanon Entropy
-      const shannonEntropyF = (val: number[]) => 
-        val.map(v => -v * Math.log2(v)).reduce((x, y) => x + y, 0);
-      
-      const entropyValue = shannonEntropyF(Object.values(stateCounts))
-      
-      // console.log(`Cell: x:${j} y:${i} Entropy: ${entropyValue}`)
-      entropy[i][j] = entropyValue
+      entropy[i][j] = cellEntropy;
     }
   }
-  
-  // let [x, y] = findBiggestValuePosition(entropy);
-  // console.log(x, y)
   let coordinate;
-  let i = -1;
-  do {
-    i++;
-    coordinate = sortEntropyArray(entropy)[i]
-  } while (hasDuplicateCoordinate(pickedCell, coordinate));
-  pickedCell.push(coordinate)
+  coordinate = sortEntropyArray(entropy)[0]
   return coordinate;
 }
+
+// function shannonEntropy(grid: Grid): Coordinate {
+//   const entropy = [...Array(grid.length)].map(e => Array(grid.length));
+//   for (let i = 0; i < grid.length; i++) {
+//     for (let j = 0; j < grid.length; j++) {
+//       const stateCounts: TerrainStates = { W: 0, L: 0, S: 0 }
+//       for (const cell of grid[i][j]) {
+//         stateCounts[cell]++;
+//       }
+//       // const neighbors = getNeighbors(grid, { x: j, y: i });
+//       // for (const neighbor of neighbors) {
+//       //   for (const cell of grid[neighbor.y][neighbor.x]) {
+//       //     stateCounts[cell]++;
+//       //   }
+//       // }
+//       // Calc Shanon Entropy
+//       const shannonEntropyF = (val: number[]) => 
+//         val.map(v => -v * Math.log2(v)).reduce((x, y) => x + y, 0);
+      
+//       const entropyValue = shannonEntropyF(Object.values(stateCounts))
+//       // console.log(stateCounts)
+//       // console.log(`Cell: x:${j} y:${i} Entropy: ${entropyValue}`)
+//       entropy[i][j] = entropyValue
+//     }
+//   }
+//   let coordinate;
+//   let i = -1;
+//   coordinate = sortEntropyArray(entropy)[i]
+//   pickedCell.push(coordinate)
+//   return coordinate;
+// }
 
 function sortEntropyArray(array: number[][]): Coordinate[] {
   // Create an array of { value, coordinate } objects
@@ -172,7 +198,8 @@ function sortEntropyArray(array: number[][]): Coordinate[] {
 
   // Sort the array by value
   data.sort((a, b) => a.value - b.value);
-  data = data.filter((e) => !isNaN(e.value));
+  // data = data.filter((e) => e.value ===);
+  data = (data.filter(e => e.value != 0));
   // Create a new array of sorted coordinates
   const sortedCoords: Coordinate[] = [];
   for (let i = 0; i < data.length; i++) {
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {  main, Terrain } from "./ASCII/ascii"
 
 console.log("Wave function Collapse Project: ASCII Implementation")
 
-main(2, {'W': 0.2, 'L': 0.5, 'S': 0.3});
+main(9, {'W': 0.2, 'L': 0.5, 'S': 0.3});
 
 
 
