9a0751a0928be23e5380e1284bc3614bbd53d307
julianjauk <jaujuc18@htl-kaindorf.at>
2023-03-24 18:23:53 +0100

    fix: working stable ✅

--- src/ASCII/ascii.ts
+++ src/ASCII/ascii.ts
@@ -20,15 +20,6 @@ type Coordinate = {
   y: number;
 }
 
-function hasDuplicateCoordinate(arr: Coordinate[], coord: Coordinate): boolean {
-  return coord === undefined ? false : arr.some(item => item.x === coord.x && item.y === coord.y);
-}
-
-
-const pickedCell = [] as Coordinate[];
-const propagatedCell = [] as Coordinate[];
-
-
 export function generateInitGrid(size: number): Grid {
    const grid: Grid = Array.from(Array(size), () => new Array(size));
   for (let i = 0; i < size; i++) {
@@ -45,30 +36,21 @@ function printGrid(grid: Grid): void {
   }
 }
 
-const getRandomCoordinate = (size: number): Coordinate => {
-  const x = Math.floor(Math.random() * size + 1);
-  const y = Math.floor(Math.random() * size + 1);
-  return { x, y };
-}
 
-
-function pickRandomUnpickedCell(grid: Grid) {
+async function pickRandomUnpickedCell(grid: Grid) {
   const coordiante = shannonEntropy(grid)
   if (coordiante === undefined) {
+    printGrid(grid)
     console.log("Done!")
     return;
   }
-  console.log("Biggest: " + coordiante.x + ", " + coordiante.y);
   const selected = grid[coordiante.y][coordiante.x];
   
   const terrain = selected[Math.floor(Math.random() * selected.length)];
   grid[coordiante.y][coordiante.x] = Array.of(terrain);
-  printGrid(grid)
-  console.log("-----------------------------------")
   grid = propagateGrid(coordiante as Coordinate, grid,);
-  printGrid(grid);
-  console.log("-----------------------------------")
   pickRandomUnpickedCell(grid);
+ 
 }
 
 
@@ -89,30 +71,39 @@ function getNeighbors(grid: Grid, target: Coordinate): Coordinate[] {
   return neighbors;
 }
 
-function setPropabilites(terrain: Terrain[]) {
-  // cell -> L -> S oder L 
-  // cell -> W -> S  oder W
-  // cell -> S -> L oder W oder S
-    // console.log(terrain)
-    switch (terrain[0]) {
-      case Terrain.Water: return [Terrain.Sand, Terrain.Water];
-      case Terrain.Sand: return [Terrain.Sand, Terrain.Water, Terrain.Land];
-      case Terrain.Land: return [Terrain.Sand, Terrain.Land];
-  }
+function isFinalState(cell: Terrain[]): boolean {
+  return cell.length === 1;
 }
 
+function setProbabilities(currentCell: Terrain[], neighbor: Terrain[]): Terrain[] {
+  if (isFinalState(neighbor)) {
+    return Array.of(neighbor[0]);
+  }
+
+  const isWater = currentCell.includes(Terrain.Water);
+  const isSand = currentCell.includes(Terrain.Sand);
+  const isLand = currentCell.includes(Terrain.Land);
+
+  if (isWater) {
+    return neighbor.includes(Terrain.Water) || neighbor.includes(Terrain.Sand)
+      ? [Terrain.Water, Terrain.Sand]
+      : [Terrain.Sand];
+  } else if (isSand) {
+    return [Terrain.Water, Terrain.Land, Terrain.Sand];
+  } else if (isLand) {
+    return neighbor.includes(Terrain.Land) || neighbor.includes(Terrain.Sand)
+      ? [Terrain.Land, Terrain.Sand]
+      : [Terrain.Sand];
+  } else {
+    return [];
+  }
+}
 function propagateGrid(coordiante: Coordinate, grid: Grid) {
-  // for (let i = 0; i < grid.length; i++) {
-  //   for (let j = 0; j < grid[i].length; j++) {
-        const currentCell = grid[coordiante.y][coordiante.x]
-  const neighbors = getNeighbors(grid, coordiante);
-        // todo: write function if the neighbor has been propagated or is in final state!
+      const currentCell = grid[coordiante.y][coordiante.x]
+      const neighbors = getNeighbors(grid, coordiante);
         for (const neighbor of neighbors) {
-          grid[neighbor.y][neighbor.x] = setPropabilites(currentCell)
+          grid[neighbor.y][neighbor.x] = setProbabilities(currentCell, grid[neighbor.y][neighbor.x])
         }
-        // propagatedCell.push({ x: j, y: i })
-  //   }
-  // }
   return grid;
 }
 
@@ -152,39 +143,7 @@ function shannonEntropy(grid: Grid) {
   return coordinate;
 }
 
-// function shannonEntropy(grid: Grid): Coordinate {
-//   const entropy = [...Array(grid.length)].map(e => Array(grid.length));
-//   for (let i = 0; i < grid.length; i++) {
-//     for (let j = 0; j < grid.length; j++) {
-//       const stateCounts: TerrainStates = { W: 0, L: 0, S: 0 }
-//       for (const cell of grid[i][j]) {
-//         stateCounts[cell]++;
-//       }
-//       // const neighbors = getNeighbors(grid, { x: j, y: i });
-//       // for (const neighbor of neighbors) {
-//       //   for (const cell of grid[neighbor.y][neighbor.x]) {
-//       //     stateCounts[cell]++;
-//       //   }
-//       // }
-//       // Calc Shanon Entropy
-//       const shannonEntropyF = (val: number[]) => 
-//         val.map(v => -v * Math.log2(v)).reduce((x, y) => x + y, 0);
-      
-//       const entropyValue = shannonEntropyF(Object.values(stateCounts))
-//       // console.log(stateCounts)
-//       // console.log(`Cell: x:${j} y:${i} Entropy: ${entropyValue}`)
-//       entropy[i][j] = entropyValue
-//     }
-//   }
-//   let coordinate;
-//   let i = -1;
-//   coordinate = sortEntropyArray(entropy)[i]
-//   pickedCell.push(coordinate)
-//   return coordinate;
-// }
-
 function sortEntropyArray(array: number[][]): Coordinate[] {
-  // Create an array of { value, coordinate } objects
   let data: { value: number, coordinate: Coordinate }[] = [];
   for (let y = 0; y < array.length; y++) {
     for (let x = 0; x < array[y].length; x++) {
@@ -192,9 +151,7 @@ function sortEntropyArray(array: number[][]): Coordinate[] {
     }
   }
 
-  // Sort the array by value
   data.sort((a, b) => a.value - b.value);
-  // data = data.filter((e) => e.value ===);
   data = (data.filter(e => e.value != 0));
   // Create a new array of sorted coordinates
   const sortedCoords: Coordinate[] = [];
@@ -204,19 +161,8 @@ function sortEntropyArray(array: number[][]): Coordinate[] {
   return sortedCoords;
 }
 
-function findBiggestValuePosition(arr: number[][]): [number, number] {
-  const flattened = arr.flat();
-  const maxValue = Math.max(...flattened);
-  const minIndex = flattened.indexOf(maxValue);
-  const numRows = arr.length;
-  const numCols = arr[0].length;
-  const minRow = Math.floor(minIndex / numCols);
-  const minCol = minIndex % numCols;
-  return [minRow, minCol];
-}
-
-
-export function main(size: number, probabilities: Record<Terrain, number>) {
+// Todo: add probabilites to the function
+export function main(size: number) {
   const grid = generateInitGrid(size);
   console.log('Initial grid:');
   printGrid(grid)
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import {  main, Terrain } from "./ASCII/ascii"
 
 console.log("Wave function Collapse Project: ASCII Implementation")
 
-main(3, {'W': 0.2, 'L': 0.5, 'S': 0.3});
+// {'W': 0.2, 'L': 0.5, 'S': 0.3}
+main(6);
 
 
 
